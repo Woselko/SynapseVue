d63a1e5e740851274f92609beab8fb0fdf353efe
DS\PTFK76 <ptfk76@motorolasoultions.com>
2024-07-06 15:25:02 +0200

    VideoStreamController allow anonymous

---
+++ src/Client/SynapseVue.Client.Core/Controllers/VideoStream/IVideoStreamController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynapseVue.Client.Core.Controllers.VideoStream;
+
+[Route("api/[controller]/[action]/")]
+public interface IVideoStreamController : IAppController
+{
+    [HttpGet]
+    public Task<int> GetVideoStream(CancellationToken cancellationToken);
+}
---
+++ src/SynapseVue.Server/Controllers/VideoStream/VideoStreamController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SynapseVue.Client.Core.Controllers.VideoStream;
+using SynapseVue.Shared.Dtos.Categories;
+
+namespace SynapseVue.Server.Controllers.VideoStream;
+
+[Route("api/[controller]/[action]")]
+[ApiController, AllowAnonymous]
+public partial class VideoStreamController : AppControllerBase, IVideoStreamController
+{
+    [HttpGet]
+    public async Task<int> GetVideoStream(CancellationToken cancellationToken)
+    {
+        return 13;
+    }
+}
